550f024ef6e75b8cb589a51545e15166d7bac91c
Burkhard Hoeckendorf <burkhard.hoeckendorf@pm.me>
2022-01-05 13:22:36 -0600

    Load UI widget only when in GUI application
    As determined by whether a Qt backend is installed.

--- src/napari_remote_interaction/__init__.py
+++ src/napari_remote_interaction/__init__.py
@@ -3,5 +3,8 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._dock_widget import napari_experimental_provide_dock_widget
+try:
+    from ._dock_widget import napari_experimental_provide_dock_widget
+except ModuleNotFoundError:
+    pass
 from .backend import RemoteInteractor
